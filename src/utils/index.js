e57daa7db50541557eca345ac009e07bb0692528
= <=>
2023-06-18 22:20:15 +0200

    Add products URL

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export const LOGIN_URL = "https://api.escuelajs.co/api/v1/auth/login";
 export const GET_PROFILE_URL = "https://api.escuelajs.co/api/v1/auth/profile";
 export const REGISTER_URL = "https://api.escuelajs.co/api/v1/users/";
+export const PRODUCTS_URL = "https://api.escuelajs.co/api/v1/products";
